c52843975c018abe750994e2fbadf67f69b95bb7
thanioruvanben10 <91356119+thanioruvanben10@users.noreply.github.com>
2023-12-31 12:53:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,10 +24,10 @@ def generate_random_string(length):
     return ''.join(random.choice(characters) for _ in range(length))
 
 def generate_and_verify_code():
-    code_prefix = input("Enter The Prefix:"))
+    code_prefix = input("Enter The Prefix:")
     if code_prefix==None:
         code_prefix = "Z5CPNA23Y"
-    xy = int(input("Enter the Suffix Digit:")
+    xy = int(input("Enter the Suffix Digit:"))
     random_suffix = generate_random_string(xy)
     code = f"{code_prefix}{random_suffix}"
 
